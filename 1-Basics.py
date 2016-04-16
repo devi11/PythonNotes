2c7eac978be8a370b369752db772992951d34c3d
sridevi <sridevi.bhavans@gmail.com>
2016-04-15 17:13:01 -0700

    Basic introduction to python

---
+++ 1-Basics.py
@@ -0,0 +1,49 @@
+# General overview
+# Python Basics
+
+print "Hello, World! "
+print ("Basic Print Statements \n")
+a= 2
+b= 3
+c= a + b
+print c
+
+one = 1
+two = 2
+three = one + two
+print three
+
+hello = "hello,"
+world = "World !"
+helloworld = hello + " " + world
+print helloworld
+
+# Numbers
+
+myint= 7
+print ("Numbers : \n Integer is %s" % myint)
+
+myfloat=7.0
+print ("Float number is %s" %myfloat)
+
+myfloat=float(7)  #float function
+print ("Using float function its %s \n " % myfloat)
+
+# Apostrophes
+
+a = "I love pop music" 	#double quote works
+b = 'I love pop music'
+c = " I’m music lover "    # when you want to include ‘ in the string
+d = 'Its\' "USA" ' # \ is escape character
+print a
+print b
+print c
+print d
+
+# Multiple Assignments
+
+a ,b = 3, 4
+c , d = "hey" , "Devi"
+
+print c + " " + d
+print "\n"
